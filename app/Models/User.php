ef2fc422037c1b096275729c35e314651b9b978e
Shibriat Hossain <ph168.sp@gmail.com>
2023-10-17 15:47:50 +0600

    Added Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'account_type',
+        'balance',
         'email',
         'password',
     ];
